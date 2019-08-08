fe3061add0387e375110a42278862d2a60f5bf91
Alex Wong <alexw14@gmail.com>
2019-08-07 23:20:33 -0700

    Add Lebron Head to App and other image assets

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 
+import LeBronHead from './assets/images/Lebron_Head_Main.png';
+
 class App extends Component {
   render() {
 
     return (
-      <div style={{backgroundColor: '#FDBA21', height: '100vh'}}>
-        <div style={{ textAlign: 'center', paddingTop: '50vh' }}>
-          We Love Taco Tuesday!!
+      <div style={{ backgroundColor: '#FDBA21' }}>
+        <div style={{ textAlign: 'center' }}>
+          <h1>Taco</h1>
+          <img src={LeBronHead} style={{ height: '230px', width: '230px' }} alt='Lebron Head'/>
+          <h1>Tuesday</h1>
         </div>
       </div>
     );
