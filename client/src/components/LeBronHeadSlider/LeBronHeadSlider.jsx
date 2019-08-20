a106fb29d5b474c7fa8e9f75d20c98982dbd5552
Alex Wong <alexw14@gmail.com>
2019-08-19 22:18:35 -0700

    Add logic to prevent slider from moving out of bounds

--- client/src/components/LeBronHeadSlider/LeBronHeadSlider.jsx
+++ client/src/components/LeBronHeadSlider/LeBronHeadSlider.jsx
@@ -1,21 +1,31 @@
 import React from 'react';
 import MainLebronHead from '../../assets/images/Lebron_Head_Default.png';
+import LeftArrow from '../../assets/images/svg/Left_Arrows_31x35_31x35.svg';
+import RightArrow from '../../assets/images/svg/Right_Arrows_31x35.svg';
 import './LeBronHeadSlider.css'
 
 const LeBronHeadSlider = (props) => {
 
     return (
-        <div>
-            <button onClick={() => props.handleMoveLeft()} style={{ cursor: 'pointer', padding: '20px' }}> Left </button>
+        <div className="game-slider-container">
             <div
-                className="LebronHeadSlider"
+                className='left-arrow'
+                onClick={() => props.handleMoveLeft()}
+            >
+                <img src={LeftArrow} />
+            </div>
+            <div
+                className='LebronHeadSlider'
                 style={{ transform: `translate(${props.pos}px, 0px)` }}
             >
-                <img style={{ height: '15vh' }}
-                    src={MainLebronHead}
-                />
+                <img src={MainLebronHead} />
             </div>
-            <button onClick={() => props.handleMoveRight()} style={{ cursor: 'pointer', padding: '20px' }}> Right </button>
+            <div
+                className='right-arrow'
+                onClick={() => props.handleMoveRight()}
+            >
+                <img src={RightArrow} />
+            </div>
         </div>
     );
 };
---
+++ client/src/components/Taco/Taco.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Taco = (props) => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default Taco;
